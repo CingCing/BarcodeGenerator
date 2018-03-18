a96a44feb61bdab9a61c42d091dd5d2667b96551
Cong <Cong@192.168.1.8>
2018-03-18 22:58:32 +0700

    generate qr code image and show qr code as image

---
+++ src/main/java/controller/generateQRCodeImage.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.zxing.WriterException;
+
+import model.QrCodeComponent;
+import service.QrCodeImage;
+
+/**
+ * Servlet implementation class generateQRCodeImage
+ */
+@WebServlet("/generateQRCodeImage")
+public class generateQRCodeImage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	QrCodeComponent qrcode = new QrCodeComponent();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public generateQRCodeImage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+//		PrintWriter out = response.getWriter();
+		
+		String qrtext = request.getParameter("qrtext");
+		qrcode.setQrtext(qrtext);
+		String text = qrcode.getQrtext();
+		String image_path = "./src/main/webapp/resources/image/" + text + ".png";
+		request.setAttribute("input", qrtext);
+		
+		try {
+			QrCodeImage.generateQRCodeImage(text, 175, 175, image_path);
+			request.getRequestDispatcher("welcome.jsp").forward(request, response);
+		} catch (WriterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
